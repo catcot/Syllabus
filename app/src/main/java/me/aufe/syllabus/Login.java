60dbb4c8725b594f2b98366655e68733ddda4222
Ivan Lee <nicecode@yeah.net>
2016-10-09 15:26:44 +0800

    make LoginActivity's status bar transparent

--- app/src/main/java/me/aufe/syllabus/Login.java
+++ app/src/main/java/me/aufe/syllabus/Login.java
@@ -1,13 +1,19 @@
 package me.aufe.syllabus;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Login extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //make status bar transparent
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.KITKAT)getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
         setContentView(R.layout.activity_login);
     }
 }
